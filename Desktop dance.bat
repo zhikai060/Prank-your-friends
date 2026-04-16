c9a035e13f8821bb783b71a396072843a9c472bd
zhikai060 <zhikai060@gmail.com>
2026-04-16 22:19:56 +0800

    Small Update

--- Desktop dance.bat
+++ Desktop dance.bat
@@ -5,7 +5,7 @@ color 0A
 echo Initializing desktop glitch...
 timeout /t 2 >nul
 
-for /L %%i in (1,1,20) do (
+for /L %%i in (1,1,6) do (
     taskkill /f /im explorer.exe >nul
     timeout /t 1 >nul
     start explorer.exe
